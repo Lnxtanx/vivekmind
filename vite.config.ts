e5833ab1b3e2a400bbd2fa7ab01a00d9e780406a
Vivek <vivek@vivekmind.com>
2026-04-21 00:27:01 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
@@ -11,6 +12,7 @@ export default defineConfig({
       viteEnvironment: { name: "ssr" },
     }),
     tanstackStart(),
+    react(),
     tailwindcss(),
     tsconfigPaths(),
   ],
